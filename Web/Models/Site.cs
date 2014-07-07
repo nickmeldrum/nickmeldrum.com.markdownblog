a34109aa8d17897b821706953f0f83ae3a52b861
Nick Meldrum <nick@nickmeldrum.com>
2014-07-07 12:14:46 +0100

    added google tag manager into layout fixes #7

--- Web/Models/Site.cs
+++ Web/Models/Site.cs
@@ -7,5 +7,6 @@ namespace MarkdownBlog.Net.Web.Models {
         public string Description { get { return ConfigurationManager.AppSettings["Description"] ?? "Default Site Description"; } }
         public string Owner { get { return ConfigurationManager.AppSettings["Owner"] ?? "Default Site Owner Name"; } }
         public string DisqusShortName { get { return ConfigurationManager.AppSettings["DisqusShortName"] ?? "DefaultDisqusShortName"; } }
+        public string GoogleTagManagerAccount { get { return ConfigurationManager.AppSettings["GoogleTagManagerAccount"] ?? "DefaultGoogleTagManagerAccount"; } }
     }
 }
