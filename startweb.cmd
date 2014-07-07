080dd9117791c43b3c98995f927e266f67fbade6
Nick Meldrum <nick@nickmeldrum.com>
2014-07-07 08:54:24 +0100

    added start dev storage to startweb.cmd for when working outside of vs references #50

--- startweb.cmd
+++ startweb.cmd
@@ -1,2 +1,3 @@
 @echo off
+"C:\Program Files (x86)\Microsoft SDKs\Windows Azure\Storage Emulator\WAStorageEmulator.exe" start
 start cmd.exe /c "C:\Program Files\IIS Express\iisexpress.exe" /site:MarkdownBlog.Net.Web
