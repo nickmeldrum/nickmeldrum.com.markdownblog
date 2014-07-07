ba642363f84fc8da3df15ceda4c676f03c98da13
Nick Meldrum <nick@nickmeldrum.com>
2014-07-07 08:15:41 +0100

    added canonical url to layout including canonical domain, all lcase + no trailing slashes

--- Web/Models/Site.cs
+++ Web/Models/Site.cs
@@ -3,6 +3,7 @@
 namespace MarkdownBlog.Net.Web.Models {
     public class Site {
         public string Name { get { return ConfigurationManager.AppSettings["Name"] ?? "Default Site Name"; } }
+        public string Domain { get { return ConfigurationManager.AppSettings["Domain"] ?? "DefaultDomain.com"; } }
         public string Description { get { return ConfigurationManager.AppSettings["Description"] ?? "Default Site Description"; } }
         public string Owner { get { return ConfigurationManager.AppSettings["Owner"] ?? "Default Site Owner Name"; } }
         public string DisqusShortName { get { return ConfigurationManager.AppSettings["DisqusShortName"] ?? "DefaultDisqusShortName"; } }
