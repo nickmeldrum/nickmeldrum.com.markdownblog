16c8d72af1f54fe5ff931002944ca7d681a0f05f
Nick Meldrum <nick@nickmeldrum.com>
2015-07-15 00:12:28 +0100

    made catalog all lowercase

--- Web/App_Start/LuceneSearchConfig.cs
+++ Web/App_Start/LuceneSearchConfig.cs
@@ -17,7 +17,7 @@ namespace MarkdownBlog.Net.Web.App_Start {
             var cloudAccount = Azure.GetStorageAccount();
 
             using (var cacheDirectory = new RAMDirectory()) {
-                using (var azureDirectory = new AzureDirectory(cloudAccount, "luceneIndex", cacheDirectory)) {
+                using (var azureDirectory = new AzureDirectory(cloudAccount, "luceneindex", cacheDirectory)) {
                     using (Analyzer analyzer = new StandardAnalyzer(Lucene.Net.Util.Version.LUCENE_30)) {
                         using (var indexWriter = new IndexWriter(azureDirectory, analyzer, true, IndexWriter.MaxFieldLength.UNLIMITED)) {
                             AddDocuments(indexWriter);
