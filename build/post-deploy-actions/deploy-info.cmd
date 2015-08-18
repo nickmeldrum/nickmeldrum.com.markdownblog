89fc1fd7bf7d4dcf12d9a7473e5d7f28685a7352
Nick Meldrum <nick@nickmeldrum.com>
2015-08-18 12:24:10 +0100

    adds commas into deployinfo to make valid json

--- build/post-deploy-actions/deploy-info.cmd
+++ build/post-deploy-actions/deploy-info.cmd
@@ -1,7 +1,7 @@
 del %DEPLOYMENT_TARGET%\deployInfo.json /F /Q
 echo { >> %DEPLOYMENT_TARGET%\deployInfo.json
-echo "branch" : "%deployment_branch%" >> %DEPLOYMENT_TARGET%\deployInfo.json
-echo "buildArgs" : "%SCM_BUILD_ARGS%" >> %DEPLOYMENT_TARGET%\deployInfo.json
+echo "branch" : "%deployment_branch%", >> %DEPLOYMENT_TARGET%\deployInfo.json
+echo "buildArgs" : "%SCM_BUILD_ARGS%", >> %DEPLOYMENT_TARGET%\deployInfo.json
 echo "time" : "%date% %time%" >> %DEPLOYMENT_TARGET%\deployInfo.json
 echo } >> %DEPLOYMENT_TARGET%\deployInfo.json
 
