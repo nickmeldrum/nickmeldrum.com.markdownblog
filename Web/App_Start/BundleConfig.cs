9e261198f33bfaf402ba0e57602cb8abd9c1b98b
Nick Meldrum <nick@nickmeldrum.com>
2015-08-26 08:05:56 +0100

    fixes scrolling on mobile and removes asides. fixes #48 and #79. using 51degrees to sniff mobile

--- Web/App_Start/BundleConfig.cs
+++ Web/App_Start/BundleConfig.cs
@@ -1,22 +1,27 @@
-﻿using System.Web.Optimization;
-using MarkdownBlog.Net.Web.Infrastructure;
+﻿namespace MarkdownBlog.Net.Web {
+    using MarkdownBlog.Net.Web.Infrastructure;
+    using System.Web.Optimization;
 
-namespace MarkdownBlog.Net.Web.App_Start {
     public class BundleConfig {
         public static void RegisterBundles(BundleCollection bundles) {
-            bundles.Add(new ScriptBundle("~/js").Include(
+            var jsBundle = new ScriptBundle("~/js").Include(
                 "~/scripts/jquery-{version}.js",
                 "~/scripts/bootstrap-{version}.js",
                 "~/scripts/jquery.validate-1.13.0.js",
                 "~/scripts/jquery-validate-bootstrap3-overrides.js",
                 "~/scripts/prettify.js",
-                "~/scripts/skrollr.js",
                 "~/scripts/runprettify.js",
-                "~/scripts/search.js",
-                "~/scripts/visuals.js"
-                ));
+                "~/scripts/search.js"
+                );
+            bundles.Add(jsBundle);
 
-            var lessBundle = new Bundle("~/content/css").Include(
+            var desktopOnlyJsBundle = new ScriptBundle("~/desktopjs").Include(
+                "~/scripts/skrollr.js",
+                "~/scripts/visuals.js"
+            );
+            bundles.Add(desktopOnlyJsBundle);
+
+            var lessBundle = new Bundle("~/css").Include(
                 "~/content/bootstrap.css",
                 "~/content/body.less",
                 "~/content/prettify.css",
