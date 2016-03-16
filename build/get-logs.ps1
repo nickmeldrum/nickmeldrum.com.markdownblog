f7c24b068fe4846c8ef920325233c2a5c3e77c4f
Nick Meldrum <nick@nickmeldrum.com>
2016-03-16 13:56:13 +0000

    adds command to get azure site logs

---
+++ build/get-logs.ps1
@@ -0,0 +1,24 @@
+$ErrorActionPreference = "Stop"
+
+pushd
+cd $psscriptroot
+
+if (test-path logs) { rmdir logs -force -recurse }
+mkdir logs\nickmeldrum -force
+mkdir logs\nickmeldrum-staging -force
+
+cd $psscriptroot
+
+cd logs\nickmeldrum
+azure site log download nickmeldrum
+unzip-here diagnostics.zip
+del .\diagnostics.zip
+
+cd $psscriptroot
+
+cd logs\nickmeldrum-staging
+azure site log download nickmeldrum-staging
+unzip-here diagnostics.zip
+del .\diagnostics.zip
+
+popd
